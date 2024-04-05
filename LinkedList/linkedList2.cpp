4449d1c0bb6a4abb023b7058f0f28003a8b0e30e
AwaisAli24 <awaisali355@gmail.com>
2024-04-05 10:51:18 -0700

    .

---
+++ LinkedList/linkedList2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    public:
+    Node(){
+        value=0;
+        next=NULL;
+    }
+    Node(int value){
+        this->value=value;
+        next=NULL;
+    }
+};
+class LinkedList{
+    Node *head;
+    public:
+    LinkedList(){
+        head=NULL;
+    }
+    void addNode(int v){
+        Node* newNode=new Node(v);
+        if(head->next==NULL){
+            head=newNode;
+            return;
+        }
+        Node* temp=head;
+
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+    }
+    void printList(){
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->value<<" ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+    LinkedList list;
+    list.addNode(5);
+    list.addNode(4);
+    list.addNode(3);
+    list.addNode(2);
+    list.printList();
+    return 0;
+}
