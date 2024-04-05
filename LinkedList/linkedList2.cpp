7eb171ff0c91835d6e0ce1d2d366679eb4105970
AwaisAli24 <awaisali355@gmail.com>
2024-04-05 12:50:53 -0700

    update

--- LinkedList/linkedList2.cpp
+++ LinkedList/linkedList2.cpp
@@ -33,20 +33,53 @@ class LinkedList{
         }
         temp->next=newNode;
     }
+    void deleteNode(int noOfNode){
+        if(head==NULL){
+            cout<<"List is empty."<<endl;
+            return;
+        }
+        Node* temp1=head,*temp2=NULL;
+        int length=1;
+        while(temp1->next!=NULL){
+            temp1=temp1->next;
+            length++;
+        }
+        if(length<noOfNode){
+            cout<<"Index out of order."<<endl;
+            return;
+        }
+        temp1=head;
+        if(noOfNode==1){
+            head=head->next;
+            delete temp1;
+            return;
+        }
+        while(noOfNode-- >1){
+            temp2=temp1;
+            temp1=temp1->next;
+
+        }
+        temp2->next=temp1->next;
+        delete temp1;
+    }
     void printList(){
         Node* temp=head;
         while(temp!=NULL){
             cout<<temp->value<<" ";
             temp=temp->next;
         }
+        cout<<endl;
     }
 };
 int main(){
     LinkedList list;
-    list.addNode(5);
-    list.addNode(4);
-    list.addNode(3);
+    list.addNode(1);
     list.addNode(2);
+    list.addNode(3);
+    list.addNode(4);
+    list.addNode(5);
+    list.printList();
+    list.deleteNode(3);
     list.printList();
     return 0;
 }
