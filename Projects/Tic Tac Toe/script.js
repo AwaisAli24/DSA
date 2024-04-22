0df457cf1254204946600f3c17481ef2c5d9535d
AwaisAli24 <awaisali355@gmail.com>
2024-04-22 07:00:28 +0500

    .

--- Projects/Tic Tac Toe/script.js
+++ Projects/Tic Tac Toe/script.js
@@ -6,10 +6,12 @@ let winningPatterns=[[0,1,2],[0,3,6],[0,4,8],
 boxes.forEach((box) => {
     box.addEventListener("click",()=>{
         if(turnX){
+            box.style.color="#E54F6D";
             box.innerText="X";
             turnX=false;
         }
         else{
+            box.style.color="#F8C630";
             box.innerText="O";
             turnX=true;
         }
