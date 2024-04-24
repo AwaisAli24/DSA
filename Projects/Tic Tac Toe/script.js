bbc3a71ca4067c547e3c6285408513b7756c1ab8
AwaisAli24 <awaisali355@gmail.com>
2024-04-24 13:16:47 +0500

    .

--- Projects/Tic Tac Toe/script.js
+++ Projects/Tic Tac Toe/script.js
@@ -1,5 +1,11 @@
 let boxes = document.querySelectorAll(".boxes");
 let turnX = true;
+let player1 = document.querySelector("#player1");
+let player2 = document.querySelector("#player2");
+let winMsg = document.querySelector("#win");
+let resetButton = document.querySelector("#resetBtn");
+let player1Wins=0;
+let player2Wins=0;
 const winPatterns = [
     [0, 1, 2],
     [0, 3, 6],
@@ -24,5 +30,44 @@ boxes.forEach((box) => {
             turnX=true;
         }
         box.disabled=true;
+        checkWinner();
     })
 })
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+      let pos1Val = boxes[pattern[0]].innerText;
+      let pos2Val = boxes[pattern[1]].innerText;
+      let pos3Val = boxes[pattern[2]].innerText;
+  
+      if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val === pos2Val && pos2Val === pos3Val) {
+          showWinner(pos1Val);
+          return true;
+        }
+      }
+    }
+  };
+const disableBoxes=()=>{
+    for(box of boxes){
+        box.disabled=true;
+    }
+}
+const showWinner=(winner)=>{
+    if(winner=='X'){
+        winMsg.innerText += player1.value;
+        player1Wins++;
+    }
+    else{
+        winMsg.innerText += player2.value;
+        player2Wins++;
+    }
+    disableBoxes();
+}
+const resetGame = ()=>{
+    for(box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+    winMsg.innerText="Winner: ";
+}
+resetButton.addEventListener("click",resetGame);
