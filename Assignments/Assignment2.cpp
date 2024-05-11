1d69c33f71d45ab51af4c681bf53545d41776201
AwaisAli24 <awaisali355@gmail.com>
2024-05-12 00:00:35 +0500

    assignment 2

---
+++ Assignments/Assignment2.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(){
+        data=0;
+        next=NULL;
+    }
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+class Stack{
+    Node* top;
+    public:
+    Stack(){
+        top=NULL;
+    }
+    void push(int d){
+        Node* newNode=new Node(d);
+        if(top==NULL){
+            top=newNode;
+            return;
+        }
+        newNode->next=top;
+        top=newNode;
+    }
+    void display(){
+        Node* temp=top;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    int peek(){
+        return top->data;
+    }
+    int pop(){
+        if(top==NULL){
+            cout<<"Stack is empty.";
+            return 0;
+        }
+        Node* temp=top;
+        top=top->next;
+        return temp->data;
+    }
+};
+int main(){
+    Stack s;
+    s.pop();
+    return 0;
+}
